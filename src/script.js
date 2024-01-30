2337ee136dad9157113cfcef941baacf8e3b5785
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-30 19:37:30 +0100

    added more logcic to the script.js file

--- src/script.js
+++ src/script.js
@@ -1,20 +1,21 @@
 // selecting elements
 
 // gelobal variables
-const API_URL =
-  'https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=652fd856-9ae7-4c74-a0f1-c78e8de3a994'
+let searchTerm = `kebab`
+const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchTerm}&key=652fd856-9ae7-4c74-a0f1-c78e8de3a994`
 
 // fetch data functions
 
 const getData = async () => {
   try {
     const recipes = await fetch(API_URL)
-    data = await recipes.json()
-    console.log(data)
+    const data = await recipes.json()
+    console.log(data.data.recipes)
   } catch (error) {
     console.log(error)
+    return { error: 'Failed to fetch data' } // Return an error object if there's an issue
   } finally {
-    console.log('this is the finilly')
+    console.log('This is the finally block')
   }
 }
 // functions
