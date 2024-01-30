033613fe3813566efcaab1b5972d5dbcb5d153a9
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-30 19:21:41 +0100

    added logic where to get the dtaf from the api

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
-
 // selecting elements
 
 // gelobal variables
+const API_URL =
+  'https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=652fd856-9ae7-4c74-a0f1-c78e8de3a994'
 
 // fetch data functions
 
+const getData = async () => {
+  try {
+    const recipes = await fetch(API_URL)
+    data = await recipes.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  } finally {
+    console.log('this is the finilly')
+  }
+}
 // functions
 
 // events
 
-
-console.log('Hello World!')
+getData()
+// console.log('Hello World!')
