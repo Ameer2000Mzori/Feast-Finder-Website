04be43e3177806c3dc1c44bcab9f814a4043dcf9
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-31 19:50:46 +0100

    selected elements + added new function called loadingAnimation and added logic to it

--- src/loadingAnimations.js
+++ src/loadingAnimations.js
@@ -1,1 +1,19 @@
-console.log('this is loading animations file')
+// selecting elements
+const MainBottomLeftWrapper = document.getElementById(
+  'Main-Bottom-Left-Wrapper'
+)
+const MainBottomRightWrapper = document.getElementById(
+  'Main-Bottom-Right-Wrapper'
+)
+// console.log('this is loading animations file')
+
+// function
+const loadingAnimation = (isLoading) => {
+  if (isLoading) {
+    console.log('is loading')
+  } else {
+    console.log('is not loading')
+  }
+}
+
+loadingAnimation(true)
