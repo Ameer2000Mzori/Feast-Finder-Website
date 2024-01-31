ea8b113113d0296c33a3e34a17750c0d70337876
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-31 18:20:38 +0100

    bug fixed when a card is clicked

--- src/selectedRecipe.js
+++ src/selectedRecipe.js
@@ -1,13 +1,13 @@
-// selecting elements
-const mealCards = document.querySelectorAll('meal-Card')
-const mainBottomLeftWrapper = document.getElementById(
-  'Main-Bottom-Left-Wrapper'
-)
+document.addEventListener('DOMContentLoaded', function () {
+  // Wrap the code in a setTimeout to wait for the elements to be created
+  setTimeout(function () {
+    const mealCards = document.querySelectorAll('#meal-Card')
+    console.log('Number of meal cards:', mealCards.length)
 
-mealCards.forEach((mealCard) => {
-  mealCard.addEventListener('click', () => {
-    console.log('clicked')
-    console.log(mealCards)
-  })
+    mealCards.forEach((card) => {
+      card.addEventListener('click', () => {
+        console.log('Card clicked:', card)
+      })
+    })
+  }, 1000) // Adjust the timeout as needed
 })
-console.log(mealCards)
