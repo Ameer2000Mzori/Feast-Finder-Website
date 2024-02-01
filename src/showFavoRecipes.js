37879da79bc12b291a3a43642a2879ce652ce761
Ameer Ameen <ameerapex496@gmail.com>
2024-02-02 00:37:05 +0100

    step 4 of fixiing the bug when trying remove item from book marks

--- src/showFavoRecipes.js
+++ src/showFavoRecipes.js
@@ -31,7 +31,7 @@ const showAndSelectRecipe = () => {
   MainBottomLeftWrapper.innerHTML = ''
 
   getFavoRecipesAndSelecte(recipiesData[0])
-  showFavoRecpicesRow()
+  showFavoRecpicesRow(recipiesData)
 }
 
 // get favo recpices and selecte recipe
@@ -90,8 +90,22 @@ const getFavoRecipesAndSelecte = (card) => {
   MainBottomRightWrapper.append(showedMealRightCard)
 }
 // show favo recipices row
-const showFavoRecpicesRow = () => {
+const showFavoRecpicesRow = (recipiesData) => {
   console.log('showed favo recipies row test')
+  recipiesData.forEach((element) => {
+    // creating element card
+    const mealCard = document.createElement('div')
+    mealCard.className =
+      'w-[95%] h-[60px] mt-2 mb-2 bg-orange-300 hover:bg-orange-200 active:bg-orange-300 cursor-pointer rounded-lg flex flex-row text-center items-center justify-between pl-2 pr-5 p-4'
+    mealCard.id = 'meal-Card'
+
+    // giving the dta to the card
+    mealCard.innerHTML = `
+<h1 id="meal-Title-1" class="font-mono font-bold text-slate-900">${element.title} </h1>
+<img id="meal-Img-1" class="w-[40px] h-[40px] rounded-full object-cover" src="${element.image_url}" alt=""/>
+`
+    MainBottomLeftWrapper.appendChild(mealCard)
+  })
 }
 
 // event listeners
