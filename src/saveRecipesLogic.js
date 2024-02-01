6cf1c9de90f14b3fc62aa818f9a7b0b977bd02c6
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-01 01:19:33 +0100

    changed showing the lement from html tree to really created html tree

--- src/saveRecipesLogic.js
+++ src/saveRecipesLogic.js
@@ -1,3 +1,42 @@
-export const saveRecipe = () => {
-  console.log('saveRecipe')
-}
+// // selecting elements
+
+// const savedRecipes = [
+//   {
+//     title: 'Apple',
+//     image_url:
+//       'https://cdn.pixabay.com/photo/2017/04/28/17/00/apple-1500000_960_720.jpg',
+//   },
+// ]
+
+// const getButtons = () => {
+//   let getButtonsInterval = setInterval(() => {
+//     let mealFvoBtns = document.querySelectorAll('#meal-Fvo-Btn')
+
+//     // functions
+//     const saveRecipe = (btn) => {
+//       let savedTitle =
+//         btn.parentElement.parentElement.lastElementChild.firstElementChild
+//           .textContent
+//       let savedImg =
+//         btn.parentElement.parentElement.firstElementChild.firstElementChild.src
+//       savedRecipes.push({
+//         title: savedTitle,
+//         image_url: savedImg,
+//       })
+
+//       console.log(savedRecipes.length, savedRecipes)
+//       console.log(mealFvoBtns)
+//     }
+
+//     // for each
+//     mealFvoBtns.forEach((btn) => {
+//       btn.addEventListener('click', () => {
+//         saveRecipe(btn)
+//       })
+//     })
+//     console.log(mealFvoBtns)
+//     clearInterval(getButtonsInterval)
+//   }, 1000)
+// }
+
+// getButtons()
--- src/selectedRecipe.js
+++ src/selectedRecipe.js
@@ -1,12 +1,12 @@
 // importing
 import { showCard } from './showCard.js'
-import { loadingAnimation } from './loadingAnimations.js'
 
 // using domcontent loaded event listener to wait untul the DOM is loaded
 export const selectedRecipe = () => {
   // Wrap the code in a setTimeout to wait for the elements to be created
   let selectedRecipeInterval = setInterval(function () {
     let mealCards = document.querySelectorAll('#meal-Card')
+
     console.log('Number of meal cards:', mealCards.length)
 
     mealCards.forEach((card) => {
