85b2da6fbe9928259524b316dab3dcbf2473b911
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-01 23:56:24 +0100

    small change may fix bugs

--- src/showFavoRecipes.js
+++ src/showFavoRecipes.js
@@ -2,6 +2,7 @@
 import { showData } from './showData.js'
 import { recipiesData } from './saveRecipesLogic.js'
 import { loadingAnimation } from './loadingAnimations.js'
+// import { showCard } from './showCard.js'
 
 // selecting elements
 const bookMarksBox = document.getElementById('book-Marks-Box')
@@ -28,58 +29,6 @@ const showFavoRecpices = () => {
       MainBottomLeftWrapper.innerHTML = ''
       showData(recipiesData)
       console.log(recipiesData)
-
-      //////////////////////////
-      let selectedCardName = recipiesData[0].title
-      console.log('selectedCardName:', selectedCardName)
-
-      let selectedCardImg = recipiesData[0].image_url
-      console.log('slectedCardImg:', selectedCardImg)
-
-      const showedMealRightCard = document.createElement('div')
-      showedMealRightCard.className = `meal-View h-[100%] w-[100%] bg-orange-100 rounded-md" id="showed-Meal-Right-Card`
-      showedMealRightCard.id = 'showed-Meal-Right-Card'
-
-      const div1 = document.createElement('div')
-      div1.className = 'w-[100%] h-[100%]'
-
-      const img = document.createElement('img')
-      img.id = 'meal-Img-2'
-      img.className = 'w-[100%] h-[100%] object-cover rounded-xl'
-      img.src = selectedCardImg
-      img.alt = ''
-
-      div1.appendChild(img)
-
-      // Create the second div with a title and a button
-      const div2 = document.createElement('div')
-      div2.className =
-        'w-[100%] h-[70px] flex flex-row text-center justify-evenly items-center absolute left-0 bottom-[10px] bg-orange-200'
-
-      const innerDiv = document.createElement('div')
-      innerDiv.className =
-        'w-[300px] h-[50px] rounded-lg bg-slate-100 flex flex-col text-center items-center justify-center'
-
-      const h1 = document.createElement('h1')
-      h1.id = 'meal-Title-2'
-      h1.className = 'text-[1.6rem] text-slate-900'
-      h1.textContent = selectedCardName
-
-      innerDiv.appendChild(h1)
-      div2.appendChild(innerDiv)
-
-      const deleteRecipeBtn = document.createElement('button')
-      deleteRecipeBtn.className =
-        'flex flex-col text-center justify-evenly items-center w-[75px] h-[75px] rounded-full bg-red-600 text-white'
-      const deleteIcon = document.createElement('i')
-      deleteIcon.className = ' fa-solid fa-trash text-[2rem]'
-      deleteRecipeBtn.appendChild(deleteIcon)
-      div2.appendChild(deleteRecipeBtn)
-
-      MainBottomRightWrapper.innerHTML = ``
-      showedMealRightCard.append(div1)
-      showedMealRightCard.append(div2)
-      MainBottomRightWrapper.append(showedMealRightCard)
     } else {
       showFavoRecpicesCount++
     }
@@ -90,3 +39,55 @@ bookMarksBox.addEventListener('click', () => {
   console.log('clicked')
   showFavoRecpices()
 })
+
+//////////////////////////
+// let selectedCardName = recipiesData[0].title
+// console.log('selectedCardName:', selectedCardName)
+
+// let selectedCardImg = recipiesData[0].image_url
+// console.log('slectedCardImg:', selectedCardImg)
+
+// const showedMealRightCard = document.createElement('div')
+// showedMealRightCard.className = `meal-View h-[100%] w-[100%] bg-orange-100 rounded-md" id="showed-Meal-Right-Card`
+// showedMealRightCard.id = 'showed-Meal-Right-Card'
+
+// const div1 = document.createElement('div')
+// div1.className = 'w-[100%] h-[100%]'
+
+// const img = document.createElement('img')
+// img.id = 'meal-Img-2'
+// img.className = 'w-[100%] h-[100%] object-cover rounded-xl'
+// img.src = selectedCardImg
+// img.alt = ''
+
+// div1.appendChild(img)
+
+// // Create the second div with a title and a button
+// const div2 = document.createElement('div')
+// div2.className =
+//   'w-[100%] h-[70px] flex flex-row text-center justify-evenly items-center absolute left-0 bottom-[10px] bg-orange-200'
+
+// const innerDiv = document.createElement('div')
+// innerDiv.className =
+//   'w-[300px] h-[50px] rounded-lg bg-slate-100 flex flex-col text-center items-center justify-center'
+
+// const h1 = document.createElement('h1')
+// h1.id = 'meal-Title-2'
+// h1.className = 'text-[1.6rem] text-slate-900'
+// h1.textContent = selectedCardName
+
+// innerDiv.appendChild(h1)
+// div2.appendChild(innerDiv)
+
+// const deleteRecipeBtn = document.createElement('button')
+// deleteRecipeBtn.className =
+//   'flex flex-col text-center justify-evenly items-center w-[75px] h-[75px] rounded-full bg-red-600 text-white'
+// const deleteIcon = document.createElement('i')
+// deleteIcon.className = ' fa-solid fa-trash text-[2rem]'
+// deleteRecipeBtn.appendChild(deleteIcon)
+// div2.appendChild(deleteRecipeBtn)
+
+// MainBottomRightWrapper.innerHTML = ``
+// showedMealRightCard.append(div1)
+// showedMealRightCard.append(div2)
+// MainBottomRightWrapper.append(showedMealRightCard)
