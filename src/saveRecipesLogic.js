b0680f96d3fd12b79af43293c481e522846f1847
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-01 01:30:25 +0100

    added logic where you cant add a recipe twice

--- src/saveRecipesLogic.js
+++ src/saveRecipesLogic.js
@@ -10,12 +10,19 @@ const savedRecipes = [
 
 // functions
 export const saveRecipe = (selectedTitle, selectedImg) => {
-  console.log('newly selected title and img : ', selectedTitle, selectedImg)
-  savedRecipes.push({
-    title: 'new recipe',
-    image_url:
-      'https://cdn.pixabay.com/photo/2017/04/28/17/00/apple-1500000_960_720.jpg',
+  let isThereRecipe = savedRecipes.some((recipes) => {
+    return recipes.title === selectedTitle
   })
 
+  if (!isThereRecipe) {
+    console.log('isThereRecipe? :', isThereRecipe)
+    console.log('newly selected title and img : ', selectedTitle, selectedImg)
+    savedRecipes.push({
+      title: selectedTitle,
+      image_url: selectedImg,
+    })
+  } else {
+    console.log('this recipe is already in savedRecipes')
+  }
   console.log('savedRecipes:', savedRecipes)
 }
