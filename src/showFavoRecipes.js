f4410793fc34012cf0262e003d420340a80c3139
Ameer Ameen <ameerapex496@gmail.com>
2024-02-02 00:31:38 +0100

    step 3 finished of fixen the bug where you are not able to remove or add new recipe when book marks clicked

--- src/showFavoRecipes.js
+++ src/showFavoRecipes.js
@@ -73,9 +73,17 @@ const getFavoRecipesAndSelecte = (card) => {
   h1.className = 'text-[1.6rem] text-slate-900'
   h1.textContent = selectedCardName
 
+  // our newly made delete button
+  const deleteRecipeBtn = document.createElement('button')
+  deleteRecipeBtn.className =
+    'flex flex-col text-center justify-evenly items-center w-[75px] h-[75px] rounded-full bg-red-600 text-white'
+  const deleteIcon = document.createElement('i')
+  deleteIcon.className = ' fa-solid fa-trash text-[2rem]'
+  deleteRecipeBtn.appendChild(deleteIcon)
+
   innerDiv.appendChild(h1)
   div2.appendChild(innerDiv)
-
+  div2.appendChild(deleteRecipeBtn)
   MainBottomRightWrapper.innerHTML = ``
   showedMealRightCard.append(div1)
   showedMealRightCard.append(div2)
@@ -91,58 +99,6 @@ bookMarksBox.addEventListener('click', () => {
   console.log('clicked')
   showFavoRecpices()
 })
-
-//////////////////////////
-// let selectedCardName = recipiesData[0].title
-// console.log('selectedCardName:', selectedCardName)
-
-// let selectedCardImg = recipiesData[0].image_url
-// console.log('slectedCardImg:', selectedCardImg)
-
-// const showedMealRightCard = document.createElement('div')
-// showedMealRightCard.className = `meal-View h-[100%] w-[100%] bg-orange-100 rounded-md" id="showed-Meal-Right-Card`
-// showedMealRightCard.id = 'showed-Meal-Right-Card'
-
-// const div1 = document.createElement('div')
-// div1.className = 'w-[100%] h-[100%]'
-
-// const img = document.createElement('img')
-// img.id = 'meal-Img-2'
-// img.className = 'w-[100%] h-[100%] object-cover rounded-xl'
-// img.src = selectedCardImg
-// img.alt = ''
-
-// div1.appendChild(img)
-
-// // Create the second div with a title and a button
-// const div2 = document.createElement('div')
-// div2.className =
-//   'w-[100%] h-[70px] flex flex-row text-center justify-evenly items-center absolute left-0 bottom-[10px] bg-orange-200'
-
-// const innerDiv = document.createElement('div')
-// innerDiv.className =
-//   'w-[300px] h-[50px] rounded-lg bg-slate-100 flex flex-col text-center items-center justify-center'
-
-// const h1 = document.createElement('h1')
-// h1.id = 'meal-Title-2'
-// h1.className = 'text-[1.6rem] text-slate-900'
-// h1.textContent = selectedCardName
-
-// innerDiv.appendChild(h1)
-// div2.appendChild(innerDiv)
-
-// const deleteRecipeBtn = document.createElement('button')
-// deleteRecipeBtn.className =
-//   'flex flex-col text-center justify-evenly items-center w-[75px] h-[75px] rounded-full bg-red-600 text-white'
-// const deleteIcon = document.createElement('i')
-// deleteIcon.className = ' fa-solid fa-trash text-[2rem]'
-// deleteRecipeBtn.appendChild(deleteIcon)
-// div2.appendChild(deleteRecipeBtn)
-
-// MainBottomRightWrapper.innerHTML = ``
-// showedMealRightCard.append(div1)
-// showedMealRightCard.append(div2)
-// MainBottomRightWrapper.append(showedMealRightCard)
 
 ////////////////////// trashed //////////////////////
 // let showFavoRecpicesCount = 0
