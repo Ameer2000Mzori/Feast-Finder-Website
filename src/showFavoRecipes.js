04f2996a2e34f2071fdbc3c4169747f59db9aa1d
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-01 16:38:31 +0100

    add logic when book mark is clicked the favo recepices will be shown

--- src/showFavoRecipes.js
+++ src/showFavoRecipes.js
@@ -1,11 +1,25 @@
+// imoprting functions
+import { showData } from 'showData.js'
+import { savedRecipes } from './saveRecipesLogic.js'
+
 // selecting elements
 const bookMarksBox = document.getElementById('book-Marks-Box')
-
+const MainBottomLeftWrapper = document.getElementById(
+  'Main-Bottom-Left-Wrapper'
+)
 // gelobal variables
+let bookMarksShown = false
 
 // functions
 
+// this function shows favo recpices
+const showFavoRecpices = () => {
+  bookMarksShown = true
+  MainBottomLeftWrapper.innerHTML = ''
+  showData(savedRecipes)
+}
 // event listeners
 bookMarksBox.addEventListener('click', (e) => {
   console.log('clicked')
+  showFavoRecpices()
 })
