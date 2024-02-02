b3b4b1dff0c9d95a94e76d04e9ac0228b9013306
Ameer Ameen <ameerapex496@gmail.com>
2024-02-02 03:04:13 +0100

    added else if lader for the notifications

--- src/notifications.js
+++ src/notifications.js
@@ -1,1 +1,12 @@
 console.log('from the notifications')
+const showNotification = (stageNotification) => {
+  if (stageNotification === 1) {
+    console.log('this is the first notification')
+  } else if (stageNotification === 2) {
+    console.log('this is the second notifications')
+  } else if (stageNotification === 3) {
+    console.log('this is the third notification')
+  } else if (stageNotification === 4) {
+    console.log('this is the fourth notification')
+  }
+}
