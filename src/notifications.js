ef3550b1a00f12ec474862e94d320cf1cf85df64
Ameer Ameen <ameerapex496@gmail.com>
2024-02-03 03:13:56 +0100

    added all logic needed for the notifications

--- src/notifications.js
+++ src/notifications.js
@@ -1,11 +1,33 @@
+// selecting elements
+const notificationsEl = document.getElementById('notifications')
 export const showNotification = (stageNotification) => {
   if (stageNotification === 1) {
-    console.warn('item  is deleted')
+    notificationsEl.textContent = 'deleted successfully'
+    notificationsEl.style.backgroundColor = 'green'
+    notificationsEl.style.color = 'white'
+    notificationsEl.style.opacity = '100'
+    notificationsEl.style.transform = 'translateY(0%)'
   } else if (stageNotification === 2) {
     console.warn('item added to favorite list')
+    notificationsEl.textContent = 'item added to favorite list'
+    notificationsEl.style.backgroundColor = 'green'
+    notificationsEl.style.color = 'white'
+    notificationsEl.style.opacity = '100'
+    notificationsEl.style.transform = 'translateY(0%)'
   } else if (stageNotification === 3) {
     console.warn('item is already in favorite list')
+    notificationsEl.textContent = 'item is already in favorite list'
+    notificationsEl.style.backgroundColor = 'green'
+    notificationsEl.style.color = 'white'
+    notificationsEl.style.opacity = '100'
+    notificationsEl.style.transform = 'translateY(0%)'
   } else if (stageNotification === 4) {
     console.warn('searching for item')
+    notificationsEl.textContent = 'searching for item'
+    notificationsEl.style.backgroundColor = 'green'
+    notificationsEl.style.color = 'white'
+    notificationsEl.style.opacity = '100'
+    notificationsEl.style.transform = 'translateY(0%)'
   }
 }
+showNotification(1)
